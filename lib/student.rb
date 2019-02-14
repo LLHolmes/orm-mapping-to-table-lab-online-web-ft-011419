083556408b70b3d8b62e737a550412b2e2212ab4
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-14 18:46:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,20 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  # Remember, you can access your database connection anywhere in this class with DB[:conn]
   
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def self.create_table(name, grade, id = nil)
+  end
+
+  def self.drop_table
+  end
+  
+  def save
+  end
+  
+  def self.create
+  end
+
 end
